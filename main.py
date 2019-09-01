f6989bb65245a35029d87ab2f46159335dd9d88d
Thom <thomas.foster@keble.ox.ac.uk>
2019-09-01 03:10:07 +0100

    comment out difference

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
                 valid_datasets = [IndividualFileDataset(fp) for fp in get_filepaths('valid')]
                 shuffle(valid_datasets)
                 valid_dataset = D.ChainDataset(valid_datasets)
-                valid_loader = D.DataLoader(valid_dataset, batch_size=9, collate_fn=collate_fn)
+                valid_loader = D.DataLoader(valid_dataset, batch_size=2, collate_fn=collate_fn)
 
                 tp = tn = fp = fn = 0
                 
@@ -105,21 +105,21 @@ if __name__ == '__main__':
                     logger.debug('Running through validation batch')
                     logger.debug('val labels shape: ', batch.labels.shape)
                     outputs = model(batch.src, batch.segs, batch.clss, batch.mask_attn, batch.mask_clss)[0] # select sent scores
-                    valid_loss += criterion(outputs, batch.labels)
+                #     valid_loss += criterion(outputs, batch.labels)
                     
-                    logger.debug(outputs)
-                    outputs = (outputs>0.5).type(torch.int)
-                    logger.debug('val outputs shape:',outputs.shape)
-                    tp += ((outputs == 1) * (batch.labels == 1)).sum().item()
-                    tn += ((outputs == 0) * (batch.labels == 0)).sum().item()
-                    fp += ((outputs == 1) * (batch.labels == 0)).sum().item()
-                    fn += ((outputs == 0) * (batch.labels == 1)).sum().item()
+                #     logger.debug(outputs)
+                #     outputs = (outputs>0.5).type(torch.int)
+                #     logger.debug('val outputs shape:',outputs.shape)
+                #     tp += ((outputs == 1) * (batch.labels == 1)).sum().item()
+                #     tn += ((outputs == 0) * (batch.labels == 0)).sum().item()
+                #     fp += ((outputs == 1) * (batch.labels == 0)).sum().item()
+                #     fn += ((outputs == 0) * (batch.labels == 1)).sum().item()
 
-                    if jdx == 100:
-                        break
+                #     if jdx == 100:
+                #         break
 
-                cf = np.array([[tp, fp],[fn, tn]])
-                logger.info(cf)
+                # cf = np.array([[tp, fp],[fn, tn]])
+                # logger.info(cf)
 
                 logger.info(f'Completed {idx} iterations, loss: {loss.item()}')
                 logger.info(f'valid_loss: {valid_loss}')
